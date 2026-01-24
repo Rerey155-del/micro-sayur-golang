56e145c56559a7363e5b70343a4938c74d2c68e3
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2026-01-24 14:45:21 +0700

    memasang config pada role

--- user-service/internal/core/domain/model/role_model.go
+++ user-service/internal/core/domain/model/role_model.go
@@ -1,1 +1,12 @@
-package	
+package model
+
+import "time"
+
+type Role struct {
+	ID        int64     `gorm:"primaryKey"`
+	Name      string
+	Users     []User `gorm:"many2many:user_role"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
--- user-service/internal/core/domain/model/user_model.go
+++ user-service/internal/core/domain/model/user_model.go
@@ -1,1 +1,20 @@
 package model
+
+import "time"
+
+type User struct {
+	ID         int       `gorm:"primaryKey"`
+	Name       string
+	Email      string
+	Password   string
+	Address    string
+	Phone      string
+	Photo      string
+	Lat        string
+	Lng        string
+	IsVerified bool
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  *time.Time
+	Roles      []Role `gorm:"many2many:user_role"`
+}
