ca44c70b5f77513623115adf677b725717e30110
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2026-04-10 21:22:56 +0700

    melakukan penyettingan pada order-services

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 -- Initialize databases for microservices
 CREATE DATABASE IF NOT EXISTS `sayur-user-service`;
 CREATE DATABASE IF NOT EXISTS `sayur-product-service`;
+CREATE DATABASE IF NOT EXISTS `sayur-order-service`;
